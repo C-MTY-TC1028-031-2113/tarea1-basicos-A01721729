476e0392530d0913e23bf8c9a38f06657ff38dda
A01721729 <88851442+A01721729@users.noreply.github.com>
2021-08-22 23:41:01 -0500

    Update exercise.py

--- assignments/03PendienteLinea/src/exercise.py
+++ assignments/03PendienteLinea/src/exercise.py
@@ -1,7 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
-    pass
+    X1 = float(input("Dame X1: "))
+    Y1 = float(input("Dame Y1: "))
+    X2 = float(input("Dame X2: "))
+    Y2 = float(input("Dame Y2: "))
+
+    m = (Y2 - Y1)/(X2 - X1)
+
+    print("Pendiente: " + str(m))
 
 
 

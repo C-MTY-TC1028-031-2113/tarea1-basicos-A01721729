f5eaff569b367769c33c47bc486adf86a3f4cc5a
A01721729 <88851442+A01721729@users.noreply.github.com>
2021-08-22 23:41:47 -0500

    Update exercise.py

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,6 +1,25 @@
 def main():
     from math import sqrt
-    pass
+    import math
+    oper1 = (2+3/4) + (4 + 2/3) - (3 + 1/5) + (5 + 1/2)
+    oper2 = 2*math.sqrt(35**2) + 4*math.sqrt(36**3) - 6*math.sqrt(49**2)
+    a = 4
+    b = 5
+    oper3 = (a**3 + 2*b**2) / (4*a)
+    oper4 = ((2*((a+b)**2) + 4*((a-b)**2)) / (a*b**2))
+    oper5 = math.sqrt((a+b)**2 + 2**(a+b)) / (2*a + 2*b)**2
+
+    oper1 = round(oper1,4)
+    oper2 = round(oper2,4)
+    oper3 = round(oper3,4)
+    oper4 = round(oper4,4)
+    oper5 = round(oper5,4)
+
+    print(oper1)
+    print(oper2)
+    print(oper3)
+    print(oper4)
+    print(oper5)
     
    
 

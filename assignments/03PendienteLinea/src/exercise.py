ca6cb1474a4ade90d4849446cb36495431dd591f
A01721729 <a01721729@itesm.mx>
2021-08-23 15:02:49 -0500

    enter

--- assignments/03PendienteLinea/src/exercise.py
+++ assignments/03PendienteLinea/src/exercise.py
@@ -1,8 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
-    pass
+    X1 = float(input("Dame X1: "))
+    Y1 = float(input("Dame Y1: "))
+    X2 = float(input("Dame X2: "))
+    Y2 = float(input("Dame Y2: "))
 
+    m = (Y2 - Y1)/(X2 - X1)
+
+    print("Pendiente: " + str(m))
+    
+    
 
 
 if __name__ == '__main__':
